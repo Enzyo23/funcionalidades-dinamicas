12496a1118bd86cfed86a2ef301cc0a720d855cd
Enzyo-dev <enzyo.50@gmail.com>
2026-09-24 23:35:35 -0300

    Configura Vite para build de producao

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+    // root: 'html' , // aqui quebrou o codigo(não utilizar assim)
+    build: {
+        rollupOptions: {
+            input: 'html/index.html'
+        }
+    }
+}
